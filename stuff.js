44ee1c8a427e714c94a2ad5c08e850fea96bf4bf
Selina Tung <hippogif04@gmail.com>
2025-10-31 09:18:17 +0000

    tying to make window drag work

--- stuff.js
+++ stuff.js
@@ -6,5 +6,32 @@ function openWindow(id) {
 
 function closeWindow(id) {
     document.getElementById(id).classList.remove('active');
-    
+
+}
+
+let draggedElement = null;
+let offsetX = 0;
+let offsetY = 0;
+
+function startDrag(e,id) {
+    draggedElement = document.getElementById(id);
+    const rect = draggedElement.getBoundingClientRect();
+    offsetX = e.clientX - rect.left;
+    offsetY = e.clientY - rect.top;
+
+    document.addEventListener('mousemove',drag);
+    document.addEventListener('mouseup', stopDrag);
+}
+
+function drag(e) {
+    if (draggedElement){
+        draggedElement.style.left = (e.clientX - offsetX) + "px";
+        draggedElement.style.top = (e.clientY - offsetY) + "px";
+    }
+}
+
+function stopDrag() {
+    draggedElement = null;
+    document.removeEventListener('mousemove',drag)
+    document.removeEventListener('mouseup', stopDrag)
 }
